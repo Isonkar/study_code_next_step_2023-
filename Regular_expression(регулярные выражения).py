fac772a4aa772d7ebf49f9c7504b1191cf448427
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-20 22:55:36 +0300

    Create Regular_expression(регулярные выражения).py

---
+++ Regular_expression(регулярные выражения).py
@@ -0,0 +1,27 @@
+import re
+
+# print(re.match())  # берет шаблон и строку и проверяет подходит ли строка под шаблон
+# print(re.search())   # берет нашу строку и находит первую подстроку которая подходит под шаблон
+# print(re.findall())  # как серч, только находит все подстроки
+# print(re.sub())      # замняет все вхождения чем нибудь другим
+
+# pattern = r'abc'
+# string = 'abc'
+# match_object = re.search(pattern, string)
+# print(match_object)
+
+pattern = r'a[abc]c'
+string = 'acc'
+match_object = re.match(pattern, string)
+print(match_object)
+
+string = 'abc, acc, aac'
+all_inclusions = re.findall(pattern, string)
+print(all_inclusions)
+
+fixed_typos = re.sub(pattern, 'abc', string) # если в строке стринг находим совпадения с паттерном, заменяем на значение 'abc
+print(fixed_typos)
+
+
+
+
