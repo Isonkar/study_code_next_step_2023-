29ab5498acfa26aa74bb90480f847f08dc2e4429
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-21 12:20:16 +0300

    Update Regular_expression(регулярные выражения).py

--- Regular_expression(регулярные выражения).py
+++ Regular_expression(регулярные выражения).py
@@ -227,3 +227,26 @@ import sys
 
 sys.stdout.writelines(re.sub('human', 'computer', sys.stdin.read()))
 #_____________________________________________________________________________________________________________
+
+'''
+Вам дана последовательность строк.
+В каждой строке замените первое вхождение слова, состоящего только из латинских букв "a" (регистр не важен), на слово "argh".
+
+Примечание:
+Обратите внимание на параметр count у функции sub.
+
+Sample Input:
+There’ll be no more "Aaaaaaaaaaaaaaa"
+AaAaAaA AaAaAaA
+
+Sample Output:
+There’ll be no more "argh"
+argh AaAaAaA
+'''
+
+import sys, re
+
+pattern = r'\b[Aa]+\b'
+for line in sys.stdin:
+    line = line.rstrip()
+    print(re.sub(pattern, 'argh', line, count=1))
