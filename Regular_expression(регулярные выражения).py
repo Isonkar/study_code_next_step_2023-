3371078f0aef526ec06308e0465bc741e86554aa
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-21 11:02:43 +0300

    Update Regular_expression(регулярные выражения).py

--- Regular_expression(регулярные выражения).py
+++ Regular_expression(регулярные выражения).py
@@ -157,3 +157,14 @@ for line in sys.stdin:
     line = line.rstrip()
     if len(re.findall(pattern, line)) > 0:
         print(line)
+        
+# вариант два
+import sys, re
+
+pattern = r'z...z'
+for line in sys.stdin:
+    line = line.rstrip()
+    if re.search(pattern, line) != None:
+        print(line)
+        
+        
