72483e3cf399f015f5cb448d2a1f6c65beed66d7
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-21 17:00:56 +0300

    Update черновик.py

--- черновик.py
+++ черновик.py
@@ -1,14 +1,23 @@
-n, x = map(int, input().split())
-mat = [[1 * i for i in range(1, n + 1)] for _ in range(1, n + 1)]
+n = int(input())
+lst = list(map(int, input().split()))
 
-cnt = 0
-for i in range(1, n):
-    for j in range(1, n):
-        mat[i][j] = i * j
-        if mat[i][j] == x:
-            cnt +=1
+mat = [[i * 0  for i in range(n)] for _ in range(n)]
+
+for i in range(n):
+    for j in range(i + 1, n):
+        mat[i][j] = lst[0]
+
+for i in range(n):
+    for j in range(i + 1, n):
+        mat[j][i] = lst[1]
+
+
+for i in range(n):
+    for j in range(n):
+      if i == j:  
+          mat[j][i] = lst[3]
+
+
 
 for i in mat:
-    print(i)
-print(cnt)
-        
+    print(*i)
