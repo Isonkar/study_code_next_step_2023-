39f9bc43afbd904bfcb3605f1dd824fa4970bda8
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-21 12:51:26 +0300

    Update Regular_expression(регулярные выражения).py

--- Regular_expression(регулярные выражения).py
+++ Regular_expression(регулярные выражения).py
@@ -300,3 +300,13 @@ pattern = r'(\w)\1+'
 for line in sys.stdin:
     line = line.rstrip()
     print(re.sub(pattern, r'\1', line))
+    
+    
+# вариант два    
+import re
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    line = re.sub(r"(\w)\1+", r"\1", line)
+    print(line)
