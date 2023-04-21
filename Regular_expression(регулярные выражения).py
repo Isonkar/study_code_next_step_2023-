7066324a0f23649db6da90ec360941eae27b081b
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-21 12:42:49 +0300

    Update Regular_expression(регулярные выражения).py

--- Regular_expression(регулярные выражения).py
+++ Regular_expression(регулярные выражения).py
@@ -250,3 +250,24 @@ pattern = r'\b[Aa]+\b'
 for line in sys.stdin:
     line = line.rstrip()
     print(re.sub(pattern, 'argh', line, count=1))
+    
+#____________________________________________________________________________________________________________
+
+'''Вам дана последовательность строк.
+В каждой строке поменяйте местами две первых буквы в каждом слове, состоящем хотя бы из двух букв.
+Буквой считается символ из группы \w.
+Sample Input:
+
+this is a text
+"this' !is. ?n1ce,
+Sample Output:
+
+htis si a etxt
+"htis' !si. ?1nce,
+'''
+import sys, re
+
+pattern = r'\b(\w)(\w)+?'
+for line in sys.stdin:
+    line = line.rstrip()
+    print(re.sub(pattern, r'\2\1', line))
