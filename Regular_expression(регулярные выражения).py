152c40d6190fae53117eefe869bd466a0a6e9412
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-21 12:50:38 +0300

    Update Regular_expression(регулярные выражения).py

--- Regular_expression(регулярные выражения).py
+++ Regular_expression(регулярные выражения).py
@@ -279,3 +279,24 @@ for line in sys.stdin:
     line = line.strip()
     line = re.sub(r"\b(\w)(\w)(\w*)\b", r"\2\1\3", line)
     print(line)
+
+#____________________________________________________________________________________________________________
+'''Вам дана последовательность строк.
+В каждой строке замените все вхождения нескольких одинаковых букв на одну букву.
+Буквой считается символ из группы \w.
+
+Sample Input:
+attraction
+buzzzz
+
+Sample Output:
+atraction
+buz
+'''
+
+import sys, re
+
+pattern = r'(\w)\1+'
+for line in sys.stdin:
+    line = line.rstrip()
+    print(re.sub(pattern, r'\1', line))
