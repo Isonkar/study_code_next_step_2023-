010f078a406af613185aae38b1b621f4e1381688
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-04-26 16:04:37 +0300

    Update list_comprehension_1.py

--- list_comprehension_1.py
+++ list_comprehension_1.py
@@ -60,5 +60,17 @@ Sample Output 2:
 
 [27, 8, 1]
 '''
+st = 'Create a list of the first letters of every word in this string'
+print([i[0] for i in list(map(str, st.split()))])
+
+'''
+Создайте список первых букв каждого слова из строки st и выведите его на экран
+st = 'Create a list of the first letters of every word in this string'
+'''
+
+
+
+
+
 
 
