b3b0078de56731c68d2e6f3c1d5360b5c0f93925
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-05-16 12:32:38 +0300

    Create def_5.py

---
+++ def_5.py
@@ -0,0 +1,40 @@
+'''
+Напишите функцию print_initials(name, surname, middlename), которая принимает три параметра:
+
+name – имя человека;
+surname – фамилия человека;
+middlename– отчество человека;
+а затем выводит на печать фамилию и инициалы в определенном формате 
+(первая буква фамилии должна стать заглавной, все остальные строчные; в имени и отчестве остаются только по одной букве в верхнем регистре).
+
+Ваша задача дописать только тело функции print_initials
+
+Sample Input 1:
+
+Мария
+Иванова
+Филлиповна
+Sample Output 1:
+
+Иванова М.Ф.
+Sample Input 2:
+
+евгЕний
+петросян
+ВаГАнович
+Sample Output 2:
+
+Петросян Е.В.
+'''
+
+# объявление функции
+def print_initials(name, surname, middlename):
+    print(f'{surname.title()} {name.title()[0]}.{middlename.title()[0]}.')
+
+# считываем данные
+name = input()
+surname = input()
+middlename = input()
+
+# вызываем функцию
+print_initials(name, surname, middlename)
