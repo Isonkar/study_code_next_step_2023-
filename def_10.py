e245892ff24d6fd9b1a2aa28dfc7fe067aba4ad6
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-05-16 17:34:11 +0300

    Update def_10.py

--- def_10.py
+++ def_10.py
@@ -1,3 +1,24 @@
+'''
+Ваша задача написать функцию get_domain_name, которая принимает строку url, извлекает из нее доменное имя и возвращает его в качестве строки
+
+get_domain_name("http://google.com") => "google"
+get_domain_name("http://google.co.jp") => "google"
+get_domain_name("www.xakep.ru") => "xakep"
+get_domain_name("https://youtube.com") => "youtube"
+get_domain_name("https://www.asos.com") => "asos"
+get_domain_name("http://www.lenovo.com") => "lenovo"
+Строка url может начинаться с протоколов http://  https:// или с www. URL, начинающиеся с протоколов http://  https://, могут также содержать www.
+
+Ваша задача написать только определение функции get_domain_name
+
+Про инструкцию assert можно прочитать здесь
+
+Sample Input:
+
+Sample Output:
+
+Проверки пройдены
+'''
 def get_domain_name(url):
     protocols = ['http://', 'https://', 'www.', 'https://www.', 'http://www.']
     for protocol in protocols:
