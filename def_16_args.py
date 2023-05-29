94bc340bac1b0b46ce68ea5df393b795fbb3ff6f
Evgeniy <123636577+Isonkar@users.noreply.github.com>
2023-05-29 14:08:50 +0300

    Create def_16_args.py

---
+++ def_16_args.py
@@ -0,0 +1,16 @@
+'''
+Напишите функцию only_one_positive, которая принимает произвольное количество числовых аргументов и возвращает True, когда из всех переданных значений только одно положительное, в противном случае верните False
+Вам необходимо написать только определение функции only_one_positive
+Ниже примеры вызова
+
+only_one_positive(1, 2) -> False
+only_one_positive(-1, 0, -3, 5, -3) -> True
+only_one_positive() -> False
+'''
+
+def only_one_positive(*args: int) -> bool:
+    cnt = 0
+    for i in args:
+        if i > 0:
+            cnt += 1
+    return True if cnt == 1 else False 
